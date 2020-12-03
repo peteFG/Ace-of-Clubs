597716af8b947afc5cff88e390f6c500554815b5
Peter Feldgrill <peter.feldgrill@edu.fh-joanneum.at>
2020-12-03 10:54:19 +0100

    changed field to fields

--- backend/backend/aceofclubs/serializers.py
+++ backend/backend/aceofclubs/serializers.py
@@ -1,34 +1,40 @@
 from rest_framework import serializers
 from . import models
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        field = ['pk', 'email', 'first_name', 'last_name', 'active',
+        fields = ['pk', 'email', 'first_name', 'last_name', 'active',
                  'permission', 'group', 'event']
+
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
-        field = ['pk', 'name', 'description', 'start_date', 'start_time',
+        fields = ['pk', 'name', 'description', 'start_date', 'start_time',
                  'end_date', 'end_time', 'active', 'group', 'eventType']
+
 
 class PermissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
-        field = ['pk', 'description']
+        fields = ['pk', 'description']
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Group
-        field = ['pk', 'name', 'description']
+        fields = ['pk', 'name', 'description']
+
 
 class EventTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.EventType
-        field = ['pk', 'description']
+        fields = ['pk', 'description']
+
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.State
-        field = ['pk', 'description']
+        fields = ['pk', 'description']
--- backend/backend/aceofclubs/views.py
+++ backend/backend/aceofclubs/views.py
@@ -9,25 +9,30 @@ class UserViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserSerializer
 
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = models.Event.objects.all().order_by('pk')
 
     serializer_class = serializers.EventSerializer
+
 
 class PermissionViewSet(viewsets.ModelViewSet):
     queryset = models.Permission.objects.all()
 
     serializer_class = serializers.PermissionSerializer
 
+
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = models.Group.objects.all()
 
     serializer_class = serializers.GroupSerializer
 
+
 class EventTypeViewSet(viewsets.ModelViewSet):
     queryset = models.EventType.objects.all()
 
     serializer_class = serializers.EventTypeSerializer
+
 
 class StateViewSet(viewsets.ModelViewSet):
     queryset = models.State.objects.all()
