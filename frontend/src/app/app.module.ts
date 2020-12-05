832b0919a19365ca17dc2141dc7ee016b2cc555e
Peter Feldgrill <peter.feldgrill@edu.fh-joanneum.at>
2020-12-05 14:18:41 +0100

    updating fixtures

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { EventListComponent } from './event-list/event-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DateComponent } from './date/date.component';
-import { TimeComponent } from './time/time.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,16 +19,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatNativeDateModule } from '@angular/material/core';
-import { EventFormComponent } from './event-form/event-form.component';
+//import { EventFormComponent } from './event-form/event-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
     DateComponent,
-    TimeComponent,
-    HomeComponent,
-    EventFormComponent
+    HomeComponent//,
+    //EventFormComponent
   ],
   imports: [
     BrowserModule,
