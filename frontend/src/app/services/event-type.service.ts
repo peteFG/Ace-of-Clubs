f234851abe3e483dfbd06b02383c3658a0ee5de5
Sophia Druckenthaner <sophia.druckenthaner@edu.fh-joanneum.at>
2020-12-06 11:41:17 +0100

    event-type service

--- frontend/src/app/services/event-type.service.ts
+++ frontend/src/app/services/event-type.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import {Interface} from "readline";
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
-/*
-export Interface EventType {
+export interface EventType {
   pk: number;
   description: string;
 }
-*/
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EventTypeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  retrieveEventTypeOptions(): Observable<EventType[]> {
+    return this.http.get<EventType[]>('/api/eventTypes/');
+  }
 }
