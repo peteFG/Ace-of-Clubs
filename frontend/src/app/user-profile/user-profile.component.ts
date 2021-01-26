3c33d3f9433dbe5154598b08e40b0d1c692152b4
Peter Feldgrill <peter.feldgrill@edu.fh-joanneum.at>
2021-01-26 20:07:02 +0100

    Minor fix for a bug that happened in the vacation function

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -13,9 +13,6 @@ import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog
 })
 export class UserProfileComponent implements OnInit {
 
-
-  // vacationFormGroup: FormGroup;
-  // vacationDateForm: FormGroup;
   pks: number[];
   userProfile: User[];
   isStaff: boolean;
@@ -29,12 +26,6 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {
 
-    /*
-    this.vacationDateForm = new FormGroup({
-      vac_start_date: new FormControl(new Date(), Validators.required),
-      vac_end_date: new FormControl(new Date(), Validators.required)
-    });
-*/
     this.retrieveUser();
 
 
@@ -58,86 +49,6 @@ export class UserProfileComponent implements OnInit {
       this.isStaff = user.is_staff;
     });
   }
-
-/*
-  private postForEveryUserEvent(): void {
-    this.pks.forEach((key) => {
-      this.vacationFormGroup = new FormGroup({
-        pk: new FormControl(),
-        user: new FormControl(this.userService.currentUserPK),
-        event: new FormControl(key),
-        state: new FormControl(3)
-      });
-      this.http.post('api/userEvent/', this.vacationFormGroup.value).subscribe(() => {
-      });
-    });
-  }
-*/
-  /*
-  createVacationForUserEvents(vac_start_date, vac_end_date): void {
-    const entriesOfActualUser = this.userService.getUserEventsOfCurrentUser();
-    this.pks = [];
-
-    this.eventService.personalEventsFunction().subscribe((events) => {
-
-      const vacationStart = vac_start_date;
-      const vacationEnd = vac_end_date; // TODO: Wenn heutiger Tag ausgewählt, dann muss man es kürzen auf normales Date Format.
-      console.log(vac_start_date);
-      console.log(vac_end_date);
-
-      events.filter(event => event.start_date.toString() >= vacationStart
-        && event.end_date.toString() < vacationEnd).forEach((entry) => {
-        this.pks.push(entry.pk);
-
-
-      });
-
-
-      entriesOfActualUser.subscribe((userEvent) => {
-        if (userEvent.length == 0) {
-
-          console.log('if check#1');
-          this.postForEveryUserEvent();
-
-
-        } else {
-          userEvent.forEach((entry) => {
-
-            if (this.pks.includes(entry.event) && entry.state != 3) {
-
-              this.vacationFormGroup = new FormGroup({
-                pk: new FormControl(entry.pk),
-                user: new FormControl(entry.user),
-                event: new FormControl(entry.event),
-                state: new FormControl(3)
-              });
-
-              this.http.put('api/userEvent/' + entry.pk + '/', this.vacationFormGroup.value).subscribe(() => {
-                alert(entry.toString() + 'has been altered!');
-
-                console.log(this.pks);
-              });
-
-
-            }
-
-            let index = this.pks.indexOf(entry.event);
-            if (index > -1) {
-              this.pks.splice(index, 1);
-            }
-            console.log(this.pks);
-          });
-          this.postForEveryUserEvent();
-
-
-        }
-        alert('Your vacation has been set!');
-
-      });
-    });
-
-  }
-*/
 
   /** VERWENDEN WENN BACKEND PROBLEM BESEITIGT */
 
@@ -188,8 +99,8 @@ export class VacationForm implements OnInit {
 
   ngOnInit(): void {
     this.vacationDateForm = new FormGroup({
-      vac_start_date: new FormControl(new Date(), Validators.required),
-      vac_end_date: new FormControl(new Date(), Validators.required)
+      vac_start_date: new FormControl(new Date().getDate(), Validators.required),
+      vac_end_date: new FormControl(new Date().getDate(), Validators.required)
     });
   }
 
@@ -213,22 +124,18 @@ export class VacationForm implements OnInit {
     this.eventService.personalEventsFunction().subscribe((events) => {
 
       const vacationStart = vac_start_date;
-      const vacationEnd = vac_end_date; // TODO: Wenn heutiger Tag ausgewählt, dann muss man es kürzen auf normales Date Format.
+      const vacationEnd = vac_end_date;
       console.log(vac_start_date);
       console.log(vac_end_date);
 
       events.filter(event => event.start_date.toString() >= vacationStart
         && event.end_date.toString() < vacationEnd).forEach((entry) => {
         this.pks.push(entry.pk);
-
-
       });
 
 
       entriesOfActualUser.subscribe((userEvent) => {
         if (userEvent.length == 0) {
-
-          console.log('if check#1');
           this.postForEveryUserEvent();
 
 
@@ -245,9 +152,6 @@ export class VacationForm implements OnInit {
               });
 
               this.http.put('api/userEvent/' + entry.pk + '/', this.vacationFormGroup.value).subscribe(() => {
-                alert(entry.toString() + 'has been altered!');
-
-                console.log(this.pks);
               });
 
 
@@ -257,7 +161,6 @@ export class VacationForm implements OnInit {
             if (index > -1) {
               this.pks.splice(index, 1);
             }
-            console.log(this.pks);
           });
           this.postForEveryUserEvent();
 
