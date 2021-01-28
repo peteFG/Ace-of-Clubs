2d2ef971c9c3e44239b0ccefdf579151b5abdf7f
TC <christoph.tripolt@edu.fh-joanneum.at>
2021-01-28 17:05:15 +0100

    even some more fixtures

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {User, UserService} from './services/user.service';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 
 
